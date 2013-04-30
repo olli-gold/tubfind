c850b311b66ccff6792f1d01d45e452ee4b2ef11
Oliver Goldschmidt <o.goldschmidt@tuhh.de>
2013-04-30 17:02:19 +0200

    removes limit of six databases for database recommender

--- sys/Recommend/DBRecommender.php
+++ sys/Recommend/DBRecommender.php
@@ -57,14 +57,15 @@ class DBRecommender {
                           JOIN dbr_database_dbis ON dbr_database.dbr_database=dbr_db_id
                           JOIN dbr_dbis ON dbr_database_dbis.dbis_id = dbr_dbis.dbis_id
                           JOIN dbr_id ON dbr_id.dbr_id=dbr_dbis.dbr_id
+                          JOIN dbis_subjects ON dbis_subjects.id=dbr_database_dbis.dbis_id
                           WHERE dbr_id.id='".$id."'
                           ORDER BY dbr_dbis.dbis_id, ranking, dbr_database.bezeichnung ASC
                     ";
                     $dbisresult = mysql_query( $query , $this->mysqlConnector );
                     while ( $row = mysql_fetch_assoc( $dbisresult ) ) {
-                        if (!in_array($row['dbr_database'], $done) && $counter < 3) {
+                        if (!in_array($row['dbr_database'], $done)) {
                             $done[] = $row['dbr_database'];
-                            $databases[] = array( 'name' => $row['bezeichnung'] , 'id' => $id , 'url' => $row['url'] , 'rank' => $data['rank'], 'group' => $row['name'] );
+                            $databases[] = array( 'name' => $row['bezeichnung'] , 'id' => $id , 'url' => $row['url'] , 'rank' => $data['rank'], 'group' => $row['subject'] );
                             $counter++;
                         }
                     }
